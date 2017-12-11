a4848c592b15ff69f9e54cc220aef5dc5d566f2e
bozdoz <howaboutben@gmail.com>
2017-12-11 15:09:34 -0400

    fixes to mobile (project list; page description; tag list)

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -6,18 +6,33 @@ const Breadcrumbs = (props) => {
 	const list = props.list;
 
 	let links = [];
+	
 	for (let i = 0, len = list.length; i < len; i+=2) {
 		let name = list[i];
 		let link = list[i + 1];
 
 		if (link) {
-			links.push((<li className="breadcrumb-item" key={name}><Link to={link}>{name}</Link></li>));
+			links.push((
+				<li className="breadcrumb-item" 
+					key={name}>
+					<Link to={link}>{name}</Link>
+				</li>
+			));
 		} else {
-			links.push((<li className="breadcrumb-item active" key={name} aria-current="page">{name}</li>));
+			// current page, no link
+			links.push((
+				<li className="breadcrumb-item active" 
+					key={name} 
+					aria-current="page">
+					{name}
+				</li>
+			));
 		}
 	}
+
 	return (
-		<nav aria-label="breadcrumb" role="navigation">
+		<nav aria-label="breadcrumb" 
+			role="navigation">
 			<ol className="breadcrumb">
 				{links}
 			</ol>
