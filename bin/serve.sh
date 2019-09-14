b045e5d1809d610ebe8206a0ce52f0063ed72a19
bozdoz <howaboutben@gmail.com>
2019-09-14 01:41:19 -0300

    possibly better deployments?

--- bin/serve.sh
+++ bin/serve.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-npm ci
+npm i
 rm ./public/js/main.js
 rm ./src/server.min.js
-npx webpack -p
-node ./src/server.min.js
+NODE_ENV=production npx webpack -p
+NODE_ENV=production node ./src/server.min.js
