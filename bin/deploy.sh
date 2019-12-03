c87e30634386533fd8aa887baa99dffd80e70e0f
bozdoz <howaboutben@gmail.com>
2019-12-03 03:57:41 -0400

    make sure to update services

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,3 +9,6 @@ cd ..
 
 docker-compose config > docker-stack.yml
 docker stack deploy -c docker-stack.yml bozdoz-${TAG:-staging}
+
+# make sure latest build is used
+docker service update --force --image registry.gitlab.com/bozdoz/bozdoz-com:${TAG:-staging} bozdoz-${TAG:-staging}_web
