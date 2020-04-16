f1a234a121ef6ee8df2f9e6b0180be196f9a7dca
Benjamin DeLong <benjamin.delong@vineview.com>
2020-04-16 01:36:24 -0300

    don't remove container src files, I guess

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 # update/copy files to static volume
 if [ -d /static ]; then
   rsync -rt /app/public/ /static/ --delete
-  rm -rf /app/public
 fi
 
 # remove cache from nginx container/volume
